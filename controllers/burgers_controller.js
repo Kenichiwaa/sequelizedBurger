4dca5245e53ae55eeadb580c0cfbc486ef20b3c4
Kenichi <Ninja@oldmacs-MacBook-Air.local>
2017-01-27 08:06:08 -0800

    enable sequelize

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,50 +7,33 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes required.
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
+
 });
 
 router.get("/burgers", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+
 });
 
+// POST route for saving a new burger
 router.post("/burgers/create", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function() {
-    res.redirect("/burgers");
+  db.Post.create(req.body).then(function(dbPost) {
+    res.json(dbPost);
   });
 });
 
 router.put("/burgers/update/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
 
-  console.log("Update condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function() {
-    res.redirect("/burgers");
-  });
 });
 
+// Delete route for deleting burgers
 router.delete("/burgers/delete/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("Delete burger "+ condition);
-
-  burger.delete(condition, function() {
-    res.redirect("/burgers");
+  db.Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbPost) {
+    res.json(dbPost);
   });
-
 });
 
 module.exports = router;
